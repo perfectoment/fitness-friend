a37b12c8974657f924e208663b19306a352b3bd3
Lee Marvin <ryenem8@gmail.com>
2020-11-07 11:46:46 -0800

    added html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,19 @@
+var path = require("path");
+
+
+
+module.exports = function(app) {
+    //Home page/Index page
+      app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+      })
+    };
+    app.get("/exercise", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+      });
+
+      app.get("/stats", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+      })
+    
+    
